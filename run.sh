8dafc90e75ab80fd2bca1e29fb3458d1c2809de7
Timofey Tagaev <vostralis@proton.me>
2025-08-28 00:00:28 +0700

    last fix i promise

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-source "$HOME/.config/waybar/waybar-schedule-module/venv/bin/activate"
-
 if [ $1 == "-f" ]; then
-    python3 fetch-schedule.py
+    $HOME/.config/waybar/waybar-schedule-module/python3 $HOME/.config/waybar/waybar-schedule-module/fetch-schedule.py
 elif [ $1 == "-d"]; then
-    python3 display-schedule.py
+    $HOME/.config/waybar/waybar-schedule-module/python3 $HOME/.config/waybar/waybar-schedule-module/display-schedule.py
 fi
